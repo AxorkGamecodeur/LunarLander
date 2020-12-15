e1880bc11f79fa91a535d3a2d1b492a77a794509
axorkgamecodeur <axorkgamecodeur@gmail.com>
2020-12-15 15:26:50 +0100

    Ajout rotation du vaisseau et affichage du moteur

--- main.lua
+++ main.lua
@@ -8,6 +8,8 @@ lander.angle = -90
 lander.vx = 0
 lander.vy = 0
 lander.img = love.graphics.newImage("images/ship.png")
+lander.imgEngine = love.graphics.newImage("images/engine.png")
+lander.engineOn = false
 
 function love.load()
     largeur = love.graphics.getWidth()
@@ -20,10 +22,25 @@ end
 function love.update(dt)
     lander.vy = lander.vy + (0.6 * dt)
 
+    if love.keyboard.isDown("right") then
+        lander.angle = lander.angle + 90 * dt
+    end
+    if love.keyboard.isDown("left") then
+        lander.angle = lander.angle - 90 * dt
+    end
+    if love.keyboard.isDown("up") then
+        engineOn = true
+    else
+        engineOn = false
+    end
+
     lander.x = lander.x + lander.vx
     lander.y = lander.y + lander.vy
 end
 
 function love.draw()
     love.graphics.draw(lander.img, lander.x, lander.y, math.rad(lander.angle), 1, 1, lander.img:getWidth()/2, lander.img:getHeight()/2)
+    if engineOn then
+        love.graphics.draw(lander.imgEngine, lander.x, lander.y, math.rad(lander.angle), 1, 1, lander.imgEngine:getWidth()/2, lander.imgEngine:getHeight()/2)
+    end
 end
