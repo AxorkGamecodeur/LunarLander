a5a8a7c241537d6e4bdcd71d339b691fd9647003
axorkgamecodeur <axorkgamecodeur@gmail.com>
2020-12-15 16:18:18 +0100

    Ajout print

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ io.stdout:setvbuf('no')
 local lander = {}
 lander.x = 0
 lander.y = 0
-lander.angle = -90
+lander.angle = 270
 lander.vx = 0
 lander.vy = 0
 lander.speed = 3
@@ -21,13 +21,19 @@ function love.load()
 end
 
 function love.update(dt)
-    lander.vy = lander.vy + (0.6 * dt)    
+    --lander.vy = lander.vy + (0.6 * dt)    
 
     if love.keyboard.isDown("right") then
         lander.angle = lander.angle + 90 * dt
+        if lander.angle > 360 then
+            lander.angle = 0
+        end
     end
     if love.keyboard.isDown("left") then
         lander.angle = lander.angle - 90 * dt
+        if lander.angle < 0 then
+            lander.angle = 360
+        end
     end
     if love.keyboard.isDown("up") then
         engineOn = true
@@ -36,10 +42,29 @@ function love.update(dt)
         local angleRadian = math.rad(lander.angle)
         local forceX = math.cos(angleRadian) * (lander.speed * dt)
         local forceY = math.sin(angleRadian) * (lander.speed * dt)
+        if math.abs(forceX) < 0.001 then forceX = 0 end
+        if math.abs(forceY) < 0.001 then forceY = 0 end
         lander.vx = lander.vx + forceX
-        lander.vy = lander.vy + forceY
+        lander.vy = lander.vy + forceY   
     else
         engineOn = false
+    end
+
+    lander.vy = lander.vy + (0.6 * dt)
+
+    if math.abs(lander.vx) > 1 then
+      if lander.vx > 0 then
+        lander.vx = 1
+      else
+        lander.vx = -1
+      end
+    end
+    if math.abs(lander.vy) > 1 then
+      if lander.vy > 0 then
+        lander.vy = 1
+      else
+        lander.vy = -1
+      end
     end
 
     lander.x = lander.x + lander.vx
@@ -52,4 +77,9 @@ function love.draw()
         love.graphics.draw(lander.imgEngine, lander.x, lander.y, math.rad(lander.angle), 1, 1, lander.imgEngine:getWidth()/2, lander.imgEngine:getHeight()/2)
     end
 
+    local sDebug = "Debug : "
+    sDebug = sDebug.."Angle="..tostring(lander.angle)
+    sDebug = sDebug.." Vx="..tostring(lander.vx)
+    sDebug = sDebug.." Vy="..tostring(lander.vy)
+    love.graphics.print(sDebug, 0, 0)
 end
